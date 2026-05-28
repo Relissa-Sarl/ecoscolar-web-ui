86c5b1ae2789bda1db908eac0ff0efc2b49c56cd
Bangumiii <pf82zkh@eduvaud.ch>
2026-05-28 15:09:51 +0200

    feat: disable SSR in production via environment variable configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: process.env.NODE_ENV !== 'production',
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
