0ab391380506c2818ccdca998ccadaf552714adf
Thomas Rey <pro.thomas.rey@gmail.com>
2026-06-16 20:13:43 +0200

    test: fix and adapte tests

--- test/nuxt/useTransactionActions.test.ts
+++ test/nuxt/useTransactionActions.test.ts
@@ -96,14 +96,15 @@ describe('useTransactionActions', () => {
   })
 
   it('executeAction dispute calls historyService when reason is provided', async () => {
-    const { promptAction, executeAction, disputeReason } = useTransactionActions()
+    const { promptAction, executeAction, disputeReason, disputeDescription } = useTransactionActions()
     mockHistoryService.disputePurchase.mockResolvedValueOnce(undefined)
 
     promptAction('dispute', 't1')
     disputeReason.value = 'Damaged item'
+    disputeDescription.value = 'It is broken'
     await executeAction()
 
-    expect(mockHistoryService.disputePurchase).toHaveBeenCalledWith('t1', 'Damaged item')
+    expect(mockHistoryService.disputePurchase).toHaveBeenCalledWith('t1', 'Damaged item', 'It is broken')
   })
 
   it('executeAction handles service errors', async () => {
