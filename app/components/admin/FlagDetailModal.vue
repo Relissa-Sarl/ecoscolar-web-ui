4c355eb14f773ebe5e1eaf653774cfa58d883a62
Thomas Rey <pro.thomas.rey@gmail.com>
2026-06-17 19:14:41 +0200

    feat: modify modal style

--- app/components/admin/FlagDetailModal.vue
+++ app/components/admin/FlagDetailModal.vue
@@ -30,13 +30,13 @@ const formatDate = (dateStr: string) => {
       v-if="isOpen"
       class="fixed flex items-center justify-center p-4"
     >
-      <div class="bg-white dark:bg-gray-900 rounded-2xl shadow-xl w-full max-w-4xl max-h-[90vh] overflow-y-auto">
+      <div class="bg-white dark:bg-gray-900 rounded-2xl shadow-xl w-full min-w-xl max-w-5xl max-h-[90vh] overflow-y-auto">
         <div class="p-6 border-b dark:border-gray-800 flex justify-between items-center">
           <h2
             id="ticket-modal-title"
             class="text-xl font-bold"
           >
-            Ticket Details
+            Flag Details
           </h2>
           <button
             class="text-gray-500 hover:text-black dark:hover:text-white"
@@ -52,35 +52,48 @@ const formatDate = (dateStr: string) => {
 
         <div class="p-6">
           <div class="space-y-4">
-            <section>
-              <h3 class="text-sm font-bold uppercase text-gray-400 mb-2">
-                User Information
-              </h3>
-              <div
-                v-if="flag"
-                class="bg-gray-50 dark:bg-gray-800 p-4 rounded-xl"
-              >
+            <div
+              v-if="flag"
+              class="bg-gray-50 dark:bg-gray-800 p-4 rounded-xl grid gap-2 grid-cols-2"
+            >
+              <p class="text-xl font-bold col-span-2">
+                Flag #{{ flag.id }}
+              </p>
+
+              <div class="col-span-2 gap-2">
                 <p class="font-bold">
-                  {{ flag.id }}
+                  {{ flag.reason.toLocaleLowerCase().replace(/^\w/, c => c.toUpperCase()).replace(/_/g, ' ') }}
                 </p>
-                <p class="text-sm text-gray-500">
-                  @{{ flag.message }}
-                </p>
-                <p class="text-sm mt-2">
-                  {{ formatDate(flag.createdAt) }}
-                </p>
-                <p
-                  class="text-xs mt-1 inline-block rounded-full bg-blue-100 text-blue-800 px-2 py-0.5 dark:bg-blue-900/50 dark:text-blue-400"
-                >
-                  User ID: {{ flag.reporterUserId }}
-                </p>
-                <p
-                  class="text-xs mt-1 inline-block rounded-full bg-blue-100 text-blue-800 px-2 py-0.5 dark:bg-blue-900/50 dark:text-blue-400"
-                >
-                  Advert ID: {{ flag.targetAdvertId }}
+                <p class="text-sm">
+                  {{ flag.message }}
                 </p>
               </div>
-            </section>
+              <div>
+                <p class="font-bold">
+                  Notification Date:
+                </p>
+                <p class="text-sm">
+                  {{ formatDate(flag.createdAt) }}
+                </p>
+              </div>
+              <div class="col-span-2">
+                <p class="font-bold">
+                  IDs:
+                </p>
+                <div class="flex">
+                  <p
+                    class="text-xs mt-1 inline-block rounded-full mr-3 bg-blue-100 text-blue-800 px-2 py-0.5 dark:bg-blue-900/50 dark:text-blue-400"
+                  >
+                    User ID: {{ flag.reporterUserId }}
+                  </p>
+                  <p
+                    class="text-xs mt-1 inline-block rounded-full ml-3 bg-blue-100 text-blue-800 px-2 py-0.5 dark:bg-blue-900/50 dark:text-blue-400"
+                  >
+                    Advert ID: {{ flag.targetAdvertId }}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
